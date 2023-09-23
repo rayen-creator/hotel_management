1b50a804ba451b8e74df0a73b97830cb58e2caf7
rayen-creator <rayenoueslati60@gmail.com>
2023-09-23 01:09:41 +0100

    💄 frontoffice layout

---
+++ client-side/src/app/components/frontoffice/frontoffice-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FrontofficeComponent } from './frontoffice.component';
+import { HomeComponent } from './shared/home/home.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { AboutComponent } from './shared/about/about.component';
+
+const routes: Routes = [{
+  path: '', component: FrontofficeComponent, children: [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: 'signin', component: SigninComponent },
+    { path: 'about', component: AboutComponent },
+
+  ]
+}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class FrontofficeRoutingModule { }
---
+++ client-side/src/app/components/frontoffice/frontoffice.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FrontofficeRoutingModule } from './frontoffice-routing.module';
+import { FrontofficeComponent } from './frontoffice.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './shared/home/home.component';
+import { AboutComponent } from './shared/about/about.component';
+
+
+@NgModule({
+  declarations: [
+    FrontofficeComponent,
+    SignupComponent,
+    SigninComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [
+    CommonModule,
+    FrontofficeRoutingModule
+  ]
+})
+export class FrontofficeModule { }
