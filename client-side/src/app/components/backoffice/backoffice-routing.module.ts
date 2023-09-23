0c6f6149e55753a5b3db28885cfe11cf79a8e012
rayen-creator <rayenoueslati60@gmail.com>
2023-09-23 10:55:14 +0100

    💄 backoffice components

--- client-side/src/app/components/backoffice/backoffice-routing.module.ts
+++ client-side/src/app/components/backoffice/backoffice-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BackofficeComponent } from './backoffice.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [{ path: '', component: BackofficeComponent }];
+const routes: Routes = [{
+  path: '', component: BackofficeComponent, children: [
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'profile', component: ProfileComponent }
+
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- client-side/src/app/components/backoffice/backoffice.module.ts
+++ client-side/src/app/components/backoffice/backoffice.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { BackofficeRoutingModule } from './backoffice-routing.module';
 import { BackofficeComponent } from './backoffice.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
-    BackofficeComponent
+    BackofficeComponent,
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProfileComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule,
