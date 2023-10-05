2c7a66485da7bf94591227e472d981cfa8249711
rayen-creator <rayenoueslati60@gmail.com>
2023-10-06 00:51:11 +0100

    ✅ added new route for support

--- client-side/src/app/components/backoffice/backoffice-routing.module.ts
+++ client-side/src/app/components/backoffice/backoffice-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { BackofficeComponent } from './backoffice.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ListTicketsComponent } from './support/list-tickets/list-tickets.component';
 
 const routes: Routes = [{
   path: '', component: BackofficeComponent, children: [
     { path: 'dashboard', component: DashboardComponent },
-    { path: 'profile', component: ProfileComponent }
+    { path: 'profile', component: ProfileComponent },
+    { path: 'support', component: ListTicketsComponent }
+
 
   ]
 }];
--- client-side/src/app/components/backoffice/backoffice.module.ts
+++ client-side/src/app/components/backoffice/backoffice.module.ts
@@ -8,6 +8,10 @@ import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { ListTicketsComponent } from './support/list-tickets/list-tickets.component';
+import { TicketCardComponent } from './support/ticket-card/ticket-card.component';
+import { CommentSectionComponent } from './support/comment-section/comment-section.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -17,11 +21,15 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
     HeaderComponent,
     FooterComponent,
     ProfileComponent,
-    SidebarComponent
+    SidebarComponent,
+    ListTicketsComponent,
+    TicketCardComponent,
+    CommentSectionComponent
   ],
   imports: [
     CommonModule,
-    BackofficeRoutingModule
+    BackofficeRoutingModule,
+    FormsModule,
   ]
 })
 export class BackofficeModule { }
