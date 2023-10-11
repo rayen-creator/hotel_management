e4aaf261174be40136275c676fa3b66a0c3819f3
rayen-creator <rayenoueslati60@gmail.com>
2023-10-11 13:15:41 +0100

    ✨ added db module

--- ms_user_management/src/module/auth/auth.module.ts
+++ ms_user_management/src/module/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './controller/auth.controller';
 import { AuthService } from './services/auth.service';
+import { DatabaseModule } from 'src/config/database/database.module';
 
 @Module({
+  imports:[DatabaseModule],
   controllers: [AuthController],
   providers: [AuthService]
 })
--- ms_user_management/src/module/user/user.module.ts
+++ ms_user_management/src/module/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './services/user.service';
 import { UserController } from './controller/user.controller';
+import { DatabaseModule } from 'src/config/database/database.module';
 
 @Module({
+  imports:[DatabaseModule],
   providers: [UserService],
   controllers: [UserController]
 })
