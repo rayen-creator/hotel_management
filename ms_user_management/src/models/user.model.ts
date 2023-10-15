1f34654f036d0738d4880e8e0c8db80757cf983b
EmnaTaalouch <emna.taalouch@esprit.tn>
2023-10-15 22:57:47 +0100

    adding login and profile function with Jwt auth access

--- ms_user_management/src/models/user.model.ts
+++ ms_user_management/src/models/user.model.ts
@@ -1,20 +1,28 @@
-import { Column, Entity } from "typeorm";
-import { BaseEntity } from "./base.entity";
-import { Role } from "./role.enum";
-
+import { Column, Entity, BeforeInsert } from 'typeorm';
+import { BaseEntity } from './base.entity';
+import { Role } from './role.enum';
+import * as bcrypt from 'bcryptjs';
 @Entity({ name: 'user' })
 export class User extends BaseEntity {
+  @Column({ type: 'varchar', unique: true })
+  username: string;
+  @Column({ type: 'varchar', unique: true })
+  email: string;
+  @Column({ type: 'varchar' })
+  password: string;
+  @Column({
+    type: 'varchar',
+    enum: Role,
+    default: Role.CLIENT,
+  })
+  userType: Role;
+  //
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 8);
+  }
 
-    @Column({ type: "varchar", unique: true })
-    username: string;
-    @Column({ type: "varchar", unique: true })
-    email: string;
-    @Column({ type: "varchar" })
-    password: string;
-    @Column({
-        type: "varchar",
-        enum: Role,
-        default: Role.CLIENT
-    })
-    userType: Role
+  async validatePassword(password: string): Promise<Boolean> {
+    return bcrypt.compare(password, this.password);
+  }
 }
--- ms_user_management/src/module/user/controller/user.controller.ts
+++ ms_user_management/src/module/user/controller/user.controller.ts
@@ -1,38 +1,49 @@
-import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UpdateUserDto } from '../dto/update-user.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { UserService } from '../services/user.service';
 
-@ApiTags("user")
+@ApiTags('user')
 @Controller('user')
-export class UserController { 
+export class UserController {
+  constructor(private readonly userService: UserService) {}
 
-    constructor(private readonly userService: UserService) { }
+  @Post('/register')
+  async create(@Body() createUserDto: CreateUserDto) {
+    return await this.userService.create(createUserDto);
+  }
 
-    @Post("/register")
-    async create(@Body() createUserDto: CreateUserDto) {
-        return await this.userService.create(createUserDto);
-    }
+  @Get('/getAllUsers')
+  async findAll() {
+    return await this.userService.findAll();
+  }
 
-    @Get("/getAllUsers")
-    async findAll() {
-        return await this.userService.findAll();
-    }
+  @Get('/getById/:id')
+  async findOne(@Param('id') id: string) {
+    return await this.userService.findOne(id);
+  }
 
-    @Get('/getById/:id')
-    async findOne(@Param('id') id: string) {
-        return await this.userService.findOne(id);
-    }
+  @Get('email/:email')
+  findByEmail(@Param('email') email: string) {
+    return this.userService.findByEmail(email);
+  }
 
-    @Put('UpdateUser/:id')
-    async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-        return await this.userService.update(id, updateUserDto);
-    }
+  @Put('UpdateUser/:id')
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return await this.userService.update(id, updateUserDto);
+  }
 
-    @Delete('/deleteuser/:id')
-    async remove(@Param('id') id: string) {
-        return await this.userService.delete(id);
-    }
+  @Delete('/deleteuser/:id')
+  async remove(@Param('id') id: string) {
+    return await this.userService.delete(id);
+  }
 }
-
