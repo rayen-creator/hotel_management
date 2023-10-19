54a5fc8825d38f7c8400cd402296b7bfcc1cf1b3
NourMakdouli <nour.makdouli@esprit.tn>
2023-10-19 16:50:00 +0100

    CRUD Service+reservation

---
+++ ms_auxiliary_services/src/main/java/tn/esprit/ms_auxiliary_services/Service/Interface/IAuxiliaryServiceService.java
@@ -0,0 +1,20 @@
+package tn.esprit.ms_auxiliary_services.Service.Interface;
+
+import tn.esprit.ms_auxiliary_services.Persistance.Entity.AuxiliaryService;
+
+import java.util.List;
+
+public interface IAuxiliaryServiceService {
+
+    public AuxiliaryService addAuxiliaryService(AuxiliaryService auxiliaryService);
+
+    AuxiliaryService updateAuxiliaryService(int id, AuxiliaryService auxiliaryService);
+
+    public List<AuxiliaryService> all_auxiliaryServices();
+    public boolean deleteAuxiliaryService(int id);
+
+    public AuxiliaryService getAuxiliaryService(int id);
+
+
+
+}
