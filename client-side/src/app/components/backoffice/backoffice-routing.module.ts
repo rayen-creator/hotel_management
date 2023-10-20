4887795f696cc33ce35fe3c790c0fe769965610d
EmnaTaalouch <emna.taalouch@esprit.tn>
2023-10-20 17:59:49 +0100

    commit

--- client-side/src/app/components/backoffice/backoffice-routing.module.ts
+++ client-side/src/app/components/backoffice/backoffice-routing.module.ts
@@ -3,7 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { BackofficeComponent } from './backoffice.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProfileComponent } from './profile/profile.component';
-<<<<<<< HEAD
+import { ListStaffComponent } from './staff-management/list-staff/list-staff.component';
+import { ListTicketsComponent } from './support/list-tickets/list-tickets.component';
+import { ItemsComponent } from './inventory/items/items.component';
+import { ItemFormComponent } from './inventory/item-form/item-form.component';
 import { ParkingComponent } from './parking/parking.component';
 import { ParkingFormComponent } from './parking-form/parking-form.component';
 import { ParkingReservationComponent } from './parking-reservation/parking-reservation.component';
@@ -15,7 +18,17 @@ const routes: Routes = [
     children: [
       { path: 'dashboard', component: DashboardComponent },
       { path: 'profile', component: ProfileComponent },
-
+      {
+        path: 'staff',
+        loadChildren: () =>
+          import('./staff-management/staff-management.module').then(
+            (m) => m.StaffManagementModule
+          ),
+      },
+      ,
+      { path: 'inventory', component: ItemsComponent },
+      { path: 'inventory/add', component: ItemFormComponent },
+      { path: 'inventory/update/:id', component: ItemFormComponent },
       { path: 'parking', component: ParkingComponent },
       { path: 'parking/add', component: ParkingFormComponent },
       { path: 'parking/update/:id', component: ParkingFormComponent },
@@ -23,25 +36,6 @@ const routes: Routes = [
     ],
   },
 ];
-=======
-import { ListStaffComponent } from './staff-management/list-staff/list-staff.component';
-import { ListTicketsComponent } from './support/list-tickets/list-tickets.component';
-import {ItemsComponent} from "./inventory/items/items.component";
-import {ItemFormComponent} from "./inventory/item-form/item-form.component";
-
-const routes: Routes = [{
-  path: '', component: BackofficeComponent, children: [
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'profile', component: ProfileComponent },
-    { path: 'staff', loadChildren: () => import('./staff-management/staff-management.module').then(m => m.StaffManagementModule) },
-,
-    { path: 'inventory', component: ItemsComponent },
-    { path: 'inventory/add', component: ItemFormComponent },
-    { path: 'inventory/update/:id', component: ItemFormComponent },
-   
-  ]
-}];
->>>>>>> 53994bf7b84dc1bbb43090bb1b6fc4e16644f3d4
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
